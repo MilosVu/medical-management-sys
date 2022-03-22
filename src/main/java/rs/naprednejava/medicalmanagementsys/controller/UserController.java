2e3c6c518fd82215eabe05170f9b5d5cd5e75c8d
Dušan Gajić <dg20170149@student.fon.bg.ac.rs>
2022-03-22 16:21:00 +0100

    edit user

--- src/main/java/rs/naprednejava/medicalmanagementsys/controller/UserController.java
+++ src/main/java/rs/naprednejava/medicalmanagementsys/controller/UserController.java
@@ -1,12 +1,17 @@
 package rs.naprednejava.medicalmanagementsys.controller;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import rs.naprednejava.medicalmanagementsys.exception.ResourceNotFoundException;
+import rs.naprednejava.medicalmanagementsys.model.Doctor;
 import rs.naprednejava.medicalmanagementsys.model.Medicine;
 import rs.naprednejava.medicalmanagementsys.model.User;
 import rs.naprednejava.medicalmanagementsys.repository.UserRepository;
@@ -38,5 +44,17 @@ public class UserController {
     public List<User> login(@RequestBody User user){
     	return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
+    
+    @DeleteMapping("/users/{id}")
+   	public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+   		User user = userRepository.findById(id)
+   				.orElseThrow(() -> new ResourceNotFoundException("User does not exist with id :" + id));
+   		
+   		userRepository.delete(user);
+   		Map<String, Boolean> response = new HashMap<>();
+   		response.put("deleted", Boolean.TRUE);
+   		return ResponseEntity.ok(response);
+   	}
+	
     
 }
